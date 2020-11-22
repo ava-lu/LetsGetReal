f978dc622737207d8020381d0a14d6949a128a1c
ava-lu <alu21@stuy.edu>
2020-11-22 12:50:03 -0500

    Updating

--- RealNumber.java
+++ RealNumber.java
@@ -16,7 +16,7 @@ public class RealNumber {
   public boolean equals(RealNumber other) {
     boolean answer = false;
     if (this.getValue() == 0 && other.getValue() == 0) answer = true;
-    double value = Math.abs((this.getValue() - other.getValue()) / other.getValue());
+    double value = Math.abs((this.getValue() - other.getValue()) / this.getValue());
     if (value < 0.00001) answer = true;
     return answer;
   }
