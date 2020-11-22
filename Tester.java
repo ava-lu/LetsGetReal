7abd56320bc25ff99ff4adafc3f6a94db1b893d5
ava-lu <alu21@stuy.edu>
2020-11-22 00:29:56 -0500

    Tested add()

--- Tester.java
+++ Tester.java
@@ -1,9 +1,10 @@
 public class Tester {
   public static void main(String[] args) {
 
-    RealNumber a = new RealNumber(0.000005);
-    RealNumber b = new RealNumber(0.0000051);
+    RealNumber a = new RealNumber(0.728);
+    RealNumber b = new RealNumber(0.22);
     System.out.println(a.equals(b));
+    System.out.println(a.add(b));
 
   }
 }
