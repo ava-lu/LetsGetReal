a8e134ec1325caf8592cb5c42b244107cb8181be
ava-lu <alu21@stuy.edu>
2020-11-22 00:21:14 -0500

    Tested equals

---
+++ Tester.java
@@ -0,0 +1,9 @@
+public class Tester {
+  public static void main(String[] args) {
+
+    RealNumber a = new RealNumber(0.000005);
+    RealNumber b = new RealNumber(0.0000051);
+    System.out.println(a.equals(b));
+
+  }
+}
