58a087effdf6c5c49d7389c2f55a1bf6ab2084f1
ava-lu <alu21@stuy.edu>
2020-11-22 01:07:15 -0500

    Tested reciprocal()

--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 public class Tester {
   public static void main(String[] args) {
 
+    /*
     RealNumber a = new RealNumber(0.728);
     RealNumber b = new RealNumber(0.22);
     System.out.println(a.equals(b));
@@ -8,5 +9,12 @@ public class Tester {
     System.out.println(a.multiply(b));
     System.out.println(a.divide(b));
     System.out.println(a.subtract(b));
+    */
+
+    RationalNumber c = new RationalNumber(1, 3);
+    RationalNumber d = new RationalNumber(2,5);
+    System.out.println(d.reciprocal().getNumerator());
+    System.out.println(d.reciprocal().getDenominator());
+
   }
 }
