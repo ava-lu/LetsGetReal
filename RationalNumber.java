d8a59cd423009fc281e210433129be1cc3f25460
ava-lu <alu21@stuy.edu>
2020-11-22 00:47:53 -0500

    Added getValue, getNumerator, getDenominator

--- RationalNumber.java
+++ RationalNumber.java
@@ -13,4 +13,15 @@ public class RationalNumber extends RealNumber {
     }
   }
 
+  public double getValue() {
+    return 0.0;
+  }
+
+  public int getNumerator() {
+    return numerator;
+  }
+
+  public int getDenominator() {
+    return denominator;
+  }
 }
