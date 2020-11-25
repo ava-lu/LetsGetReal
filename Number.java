6900a477cacc8c7d7f9fcb7ba4f41d86352a172e
ava-lu <alu21@stuy.edu>
2020-11-24 20:28:39 -0500

    Added compareTo()

--- Number.java
+++ Number.java
@@ -1,6 +1,14 @@
 public abstract class Number {
   public abstract double getValue();
 
+  public int compareTo(Number other) {
+    int comparing = 0;
+    if (this.getValue() == other.getValue()) comparing = 0;
+    if (this.getValue() < other.getValue()) comparing = -1;
+    if (this.getValue() > other.getValue()) comparing = 1;
+    return comparing;
+  }
+
   public boolean equals(Number other) {
     boolean answer = false;
     if (this.getValue() == 0 && other.getValue() == 0) answer = true;
