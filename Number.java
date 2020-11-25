6e545abc22923e14ec39a35924fea58cc43cec50
ava-lu <alu21@stuy.edu>
2020-11-24 20:07:17 -0500

    Moved equals()

--- Number.java
+++ Number.java
@@ -1,4 +1,12 @@
 public abstract class Number {
   public abstract double getValue();
 
+  public boolean equals(Number other) {
+    boolean answer = false;
+    if (this.getValue() == 0 && other.getValue() == 0) answer = true;
+    double value = Math.abs((this.getValue() - other.getValue()) / this.getValue());
+    if (value < 0.00001) answer = true;
+    return answer;
+  }
+
 }
